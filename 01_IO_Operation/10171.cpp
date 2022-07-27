8efac3913ab4060cb392e7229367d4e3d7a129de
DongYoon Kang <canfrog@naver.com>
2022-07-27 13:39:38 +0900

    Initial Commits

---
+++ 01_IO_Operation/10171.cpp
@@ -0,0 +1,14 @@
+// 고양이
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "\\    /\\ " << endl;
+	cout << " )  ( ') " << endl;
+	cout << "(  /  ) " << endl;
+	cout << " \\(__)|" << endl;
+
+	return 0;
+}
---
+++ 01_IO_Operation/2557.cpp
@@ -0,0 +1,11 @@
+// Hello World
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Hello World" << std::endl;
+	
+	return 0;
+}
